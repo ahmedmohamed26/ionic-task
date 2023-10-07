91dabed8e075233a6e3d52aaa396bee2090775a0
ahmedmohamed26 <ahmedmohamed26>
2023-10-07 14:06:14 +0300

    add subscribe  and discover section

--- src/app/screens/home/home.page.ts
+++ src/app/screens/home/home.page.ts
@@ -6,7 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  discoverArr: any = [
+    {
+      icon: 'assets/icon/subscribe.svg',
+      title: 'Step 1: Subscribe',
+      des: "Select a subscription plan that suits your child's learning needs and preferences.",
+    },
+    {
+      icon: 'assets/icon/personalise.svg',
+      title: 'Step 2: Personalise Your Box',
+      des: "Tell us about your child's age, interests, and learning goals, and we'll customize their surprise box accordingly.",
+    },
+    {
+      icon: 'assets/icon/receive.svg',
+      title: 'Step 3: Receive Your Surprise Box',
+      des: 'Sit back and relax as your child eagerly awaits the arrival of their monthly surprise box filled with engaging learning materials.',
+    },
+  ];
   constructor() {}
-
 }
